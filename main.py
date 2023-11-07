cd0ae8b68384f9547b238037f5df5f29c19907c3
jazir788 <jazirsx5@gmail.com>
2023-11-07 11:53:33 +0000

    Added sending email functionality

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 import selectorlib
+import smtplib, ssl
+import config
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
 }
-
 def scrape(url):
     response = requests.get(url, headers = HEADERS)
     source = response.text
@@ -16,8 +17,14 @@ def extract(source):
     value = extractor.extract(source)["tours"]
     return value
 
-def send_email():
-    print("Email was sent")
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(config.username, config.password)
+        server.sendmail(config.username, config.receiver, message)
+
 
 def store(extracted):
     with open("data.txt","a" ) as file:
@@ -32,8 +39,10 @@ if __name__ == "__main__":
     extracted = extract(scraped)
     print(extracted)
     content = read(extracted)
+    message = (f"Subject: New event was found \n"
+               f"{extracted} ")
     if extracted != "No upcoming tours":
         if extracted not in content:
             store(extracted)
-            send_email()
+            send_email(message=f"{message}")
 
